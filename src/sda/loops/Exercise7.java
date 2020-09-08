05472c0da4ecbe06135851fa503bc4ca1637ca70
Robert Pietrzykowski <robert.pietrzykowski@pierce-ecom.com>
2020-09-08 17:41:07 +0200

    Add new solutions and extend obj folder with class.

--- src/sda/loops/Exercise7.java
+++ src/sda/loops/Exercise7.java
@@ -1,2 +1,20 @@
-package sda.loops;public class Exercise7 {
+package sda.loops;
+
+/*
+Napisz program, który:
+
+- Pobierze od użytkownika liczbę elementów do pobrania.
+- Pozwoli wpisać taką samą liczbę różnych liczb jak liczba elementów podana wcześniej.
+- Znajdzie najwyższą liczbę podzielną przez 4 i wyświetli ją na ekranie spośród tych liczb.
+
+Liczba elementów do wpisania nie może być większa niż 1000.
+Liczba wprowadzona jako pojedyńczy element nie może być większa niż 30000.
+
+*/
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        //write your code
+    }
 }
--- src/sda/loops/Sol7.java
+++ src/sda/loops/Sol7.java
@@ -1,2 +1,29 @@
-package sda.loops;public class Sol7 {
+package sda.loops;
+
+/*
+Napisz program, który:
+
+- Pobierze od użytkownika liczbę elementów do pobrania.
+- Pozwoli wpisać taką samą liczbę różnych liczb jak liczba elementów podana wcześniej.
+- Znajdzie najwyższą liczbę podzielną przez 4 i wyświetli ją na ekranie spośród tych liczb.
+
+Liczba elementów do wpisania nie może być większa niż 1000.
+Liczba wprowadzona jako pojedyńczy element nie może być większa niż 30000.
+
+*/
+
+import java.util.Scanner;
+
+public class Sol7 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int divisibleByFour = 0;
+        while (userInput.hasNext()) {
+            int num = userInput.nextInt();
+            if (num % 4 == 0 && divisibleByFour < num) {
+                divisibleByFour = num;
+            }
+        }
+        System.out.println(divisibleByFour);
+    }
 }
